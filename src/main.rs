a1831fb5e56a1d1d622fac5caeea3b238fe2ede8
Jerry Yang <hy0kle@gmail.com>
2015-12-09 15:36:28 +0800

    完成json配置文件解析.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+extern crate rustc_serialize;
+use rustc_serialize::json::{self, Json, ToJson};
+
 use std::fmt;
 use std::env;
+
+use std::fs::File;
+use std::io::Read;
+
 
 struct WorkConf {
     project: String,
@@ -14,12 +21,23 @@ impl fmt::Debug for WorkConf {
     }
 }
 
+// Automatically generate `RustcDecodable` and `RustcEncodable` trait
+// implementations
+#[derive(RustcDecodable, RustcEncodable)]
+struct DeployConf {
+    user: String,
+    path: String,
+    hosts_conf: Vec<String>,
+}
+
 fn usage(argv_0: &String) {
     println!("-----USAGE----");
     println!("{} project deploy             deploy project with latest <head>.", argv_0);
     println!("{} project rollback <head>    rollback with <head>.", argv_0);
     std::process::exit(0);
 }
+
+static DEBUG: bool = true;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -39,5 +57,21 @@ fn main() {
         operate: operate,
         head:    head,
     };
-    //println!("{:?}", work_conf);
+    if DEBUG { println!("{:?}", work_conf); }
+
+    let config_filename = format!("./conf/{}.json", work_conf.project);
+    if DEBUG { println!("config_filename: {}", config_filename); }
+
+    let mut data = String::new();
+    let mut f = File::open(config_filename)
+        .ok()
+        .expect("Can NOT open file.");
+    f.read_to_string(&mut data)
+        .ok()
+        .expect("Can NOT read cofing file.");
+    if DEBUG { println!("deploy_conf: {}", data)}
+    let deploy_conf: DeployConf = json::decode(&data)
+        .ok()
+        .expect("Can NOT parse JSON config data.");
+    if DEBUG { println!("JSON: {}", json::encode(&deploy_conf).unwrap()); }
 }
