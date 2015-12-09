bffa80834f14e71ae595f626a9d3110423cc6b2e
Jerry Yang <hy0kle@gmail.com>
2015-12-09 17:23:04 +0800

    完成第一版.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate rustc_serialize;
-use rustc_serialize::json::{self, Json, ToJson};
+use rustc_serialize::json;
 
 use std::fmt;
 use std::env;
@@ -7,6 +7,8 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 
+use std::thread;
+use std::process::Command;
 
 struct WorkConf {
     project: String,
@@ -50,6 +52,10 @@ fn main() {
     // 处理命令行参数
     let project = args[1].to_string();
     let operate = args[2].to_string();
+    if ("deploy".to_string() != operate && "rollback".to_string() != operate) || ("rollback".to_string() == operate && argc < 4) {
+        usage(&args[0]);
+    }
+
     let head = if argc > 3 { args[3].to_string() } else { "".to_string() };
 
     let work_conf = WorkConf{
@@ -74,4 +80,39 @@ fn main() {
         .ok()
         .expect("Can NOT parse JSON config data.");
     if DEBUG { println!("JSON: {}", json::encode(&deploy_conf).unwrap()); }
+
+    let mut hosts_conf  = Vec::new();
+    let hosts_len = deploy_conf.hosts_conf.len();
+    for index in 0 .. hosts_len {
+        hosts_conf.push(deploy_conf.hosts_conf[index].clone());
+    }
+    let handles: Vec<_> = hosts_conf.into_iter().map(|host| {
+        let user = deploy_conf.user.clone();
+        let path = deploy_conf.path.clone();
+        let operate = work_conf.operate.clone();
+        let head    = work_conf.head.clone();
+        thread::spawn(move || {
+            let cmd = if "deploy".to_string() == operate {
+                format!("ssh {}@{} \"cd {} && git pull && git log -1 | awk '{{if (\\$1 ~/commit/) {{print \\$2}}}}' 2>&1\"",
+                user, host, path)
+            } else {
+                format!("ssh {}@{} \"cd {} && git reset --hard {} && git log -1 | awk '{{if (\\$1 ~/commit/) {{print \\$2}}}}' 2>&1\"",
+                user, host, path, head)
+            };
+            if DEBUG { println!("CMD: {}", cmd); }
+            let output = Command::new("sh")
+                .arg("-c")
+                .arg(cmd)
+                .output()
+                .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
+
+            if DEBUG { println!("status: {}", output.status); }
+            println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+            println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
 }
